331651c6c2eea0456df606a1444012575705e72b
Chris Skogstad <chris.skogstad1@gmail.com>
2021-08-10 21:49:36 +1000

    created and tested email generating route with nodemailer to be able to schedule emails to myself to remind who owes what

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -43,6 +43,20 @@ export default function Login() {
         uuid: ''
     })
 
+    const [sent, setSent] = useState(false);
+    const [text, setText] = useState('');
+
+    const handleSend = async (e) => {
+        setSent(true)
+        try {
+            await axios.post('http://localhost:3001/api/send_mail', {
+                text
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
 
     // console.log(selectedPlayer)
@@ -167,7 +181,15 @@ export default function Login() {
                 }
             </div>
 
+            {!sent ? (
+                <form onSubmit={handleSend}>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                    <button type='submit'>Send Email</button>
 
+                </form>
+            ) : (
+                <h1>Email Sent</h1>
+            )}
         </LoginStyles>
     )
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+require("dotenv").config()
 const { Player, Team } = require('../models');
 
 
@@ -77,5 +79,34 @@ router.put('/players/:uuid', async (req, res) => {
     }
 })
 
+router.post("/send_mail", cors(), async (req, res) => {
+    let { text } = req.body
+    const transport = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        }
+    })
+    await transport.sendMail({
+        from: process.env.MAIL_FROM,
+        to: "test@test.com",
+        subject: "test email",
+        html: `<div className="email" style="
+        border: 1px solid black;
+        padding: 20px;
+        font-family: sans-serif;
+        line-height: 2;
+        font-size: 20px; 
+        ">
+        <h2>Here is your email!</h2>
+        <p>${text}</p>
+    
+        <p>All the best, Christopher</p>
+         </div>
+    `
+    })
+})
 
 module.exports = router;
